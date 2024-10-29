c7c49a6ef88afaad0334b1d374ca413235939b59
LifeIsOne <didtmdgh258@gmail.com>
2024-10-29 20:14:07 +0900

    [REFACTORING] API프로젝트 정리

--- vue-use-api/src/main.js
+++ vue-use-api/src/main.js
@@ -1,7 +1,7 @@
 import './assets/main.css';
 
 import { createApp } from 'vue';
-import App from './App.vue';
+import App from './App_CompositionAPI.vue';
 
 createApp(App).mount('#app');
 console.log('안녕하세요');
