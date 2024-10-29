00cc0f1d4d3c6e6ea515cb282bdf5a234f6e9395
LifeIsOne <didtmdgh258@gmail.com>
2024-10-29 20:42:12 +0900

    [RENAME] 프로젝트 정리

---
+++ vue-composition-api/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<h1>기본형 메세지</h1>
+		<p>{{ nomalMsg }}</p>
+		<h1>반응형 메세지</h1>
+		<p>{{ reactiveMsg }}</p>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		const nomalMsg = '기본형 메세지';
+		const reactiveMsg = ref('반응형 메세지');
+		return {
+			nomalMsg,
+			reactiveMsg,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
