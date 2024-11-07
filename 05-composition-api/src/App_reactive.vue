54a864cbfbd23b3d0757317169342455e6fa9160
LifeIsOne <didtmdgh258@gmail.com>
2024-11-07 19:59:02 +0900

    [FEAT] reative함수로 상태관리하기

---
+++ 05-composition-api/src/App_reactive.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<button @click="increment">count : {{ state.count }}</button>
+		<button @click="increment">deep count : {{ state.deep.count }}</button>
+	</div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+	setup() {
+		const state = reactive({
+			count: 0,
+			deep: {
+				count: 0,
+			},
+		});
+		const increment = () => {
+			state.count++;
+			state.deep.count++;
+		};
+
+		return {
+			state,
+			increment,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
+<style>
+@import './assets/base.css';
+</style>
