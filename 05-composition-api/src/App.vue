0e5c2536dffe23bdfa947cbe04bc6b64ab503a91
LifeIsOne <didtmdgh258@gmail.com>
2024-11-13 20:48:55 +0900

    [FEAT] v-bind:class 클래스를 동적으로 바인딩하기

--- 05-composition-api/src/App.vue
+++ 05-composition-api/src/App.vue
@@ -1,11 +1,25 @@
 <template>
-	<div></div>
+	<div>
+		<div class="text" :class="{ active: isActive }">텍스트 입니다.</div>
+		<button @click="toggle">토글 ( toggle )</button>
+	</div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
 	setup() {
-		return {};
+		const isActive = ref(true);
+
+		const toggle = () => {
+			isActive.value = !isActive.value;
+		};
+
+		return {
+			isActive,
+			toggle,
+		};
 	},
 };
 </script>
@@ -13,4 +27,8 @@ export default {
 <style lang="scss" scoped></style>
 <style>
 @import './assets/base.css';
+.active {
+	color: #fd5;
+	font-weight: bold;
+}
 </style>
