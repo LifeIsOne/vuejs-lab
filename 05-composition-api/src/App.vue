9cdbaf8fb43fcc5807111853bd1d0dc490321490
LifeIsOne <didtmdgh258@gmail.com>
2024-11-13 19:28:16 +0900

    [FEAT] computed API 사용, 일반 메서드와 비교

--- 05-composition-api/src/App.vue
+++ 05-composition-api/src/App.vue
@@ -1,11 +1,44 @@
 <template>
-	<div></div>
+	<div>
+		<h2>{{ user.name }}</h2>
+		<h3>기술 스택이 있습니까?</h3>
+		<!-- <p>{{ user.skill.length > 0 ? '넵!' : '아니요.' }}</p> -->
+		<p>{{ hasSkill }}</p>
+		<p>{{ hasSkill }}</p>
+		<p>{{ existSkill() }}</p>
+		<p>{{ existSkill() }}</p>
+		<button @click="counter++">클릭하기! {{ counter }}</button>
+	</div>
 </template>
 
 <script>
+import { computed, reactive, ref } from 'vue';
+
 export default {
 	setup() {
-		return {};
+		const user = reactive({
+			name: 'matthew',
+			skill: ['Java', 'JavaScript', 'Vue3'],
+		});
+
+		const hasSkill = computed(() => {
+			console.log('computed');
+			return user.skill.length > 0 ? '넵!' : '아니요.';
+		});
+
+		const existSkill = () => {
+			console.log('method');
+			return user.skill.length > 0 ? '넵!' : '아니요.';
+		};
+
+		const counter = ref(0);
+
+		return {
+			user,
+			hasSkill,
+			existSkill,
+			counter,
+		};
 	},
 };
 </script>
