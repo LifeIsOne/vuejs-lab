e0c1341c1b244269afad605da9482489be40b488
LifeIsOne <didtmdgh258@gmail.com>
2024-11-13 18:05:15 +0900

    [FEAT] toRef() / toRefs() API 구조 분해 할당 객체 반응형 상태 유지하기

--- 05-composition-api/src/App.vue
+++ 05-composition-api/src/App.vue
@@ -1,11 +1,27 @@
 <template>
-	<div></div>
+	<div>
+		<p>user: {{ user }}</p>
+		<p>title: {{ title }}</p>
+	</div>
 </template>
 
 <script>
+import { reactive, toRef, toRefs } from 'vue';
+
 export default {
 	setup() {
-		return {};
+		const board = reactive({
+			user: 'Matthew',
+			data: '2024-11-13',
+			title: '제목1',
+			content: '내용1',
+		});
+
+		// const { user, title } = toRefs(board);
+		const user = toRef(board, 'user');
+		const title = toRef(board, 'title');
+
+		return { user, title, board };
 	},
 };
 </script>
