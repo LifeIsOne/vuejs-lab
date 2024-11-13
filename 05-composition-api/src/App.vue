12c42d717f17f9487c18ce093087021abb61ef18
LifeIsOne <didtmdgh258@gmail.com>
2024-11-13 18:07:37 +0900

    [RENAME] 코드 네이밍 변경

--- 05-composition-api/src/App.vue
+++ 05-composition-api/src/App.vue
@@ -1,27 +1,11 @@
 <template>
-	<div>
-		<p>user: {{ user }}</p>
-		<p>title: {{ title }}</p>
-	</div>
+	<div></div>
 </template>
 
 <script>
-import { reactive, toRef, toRefs } from 'vue';
-
 export default {
 	setup() {
-		const board = reactive({
-			user: 'Matthew',
-			data: '2024-11-13',
-			title: '제목1',
-			content: '내용1',
-		});
-
-		// const { user, title } = toRefs(board);
-		const user = toRef(board, 'user');
-		const title = toRef(board, 'title');
-
-		return { user, title, board };
+		return {};
 	},
 };
 </script>
---
+++ 05-composition-api/src/App_toRef_toRefs.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<p>user: {{ user }}</p>
+		<p>title: {{ title }}</p>
+	</div>
+</template>
+
+<script>
+import { reactive, toRef, toRefs } from 'vue';
+
+export default {
+	setup() {
+		const board = reactive({
+			user: 'Matthew',
+			data: '2024-11-13',
+			title: '제목1',
+			content: '내용1',
+		});
+
+		// const { user, title } = toRefs(board);
+		const user = toRef(board, 'user');
+		const title = toRef(board, 'title');
+
+		return { user, title, board };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
+<style>
+@import './assets/base.css';
+</style>
