19c87ba021f12c4bbba5994fee0b969bdbdef93f
LifeIsOne <didtmdgh258@gmail.com>
2024-11-13 22:48:36 +0900

    [FEAT] v-else-if : 여러 조건 연결

--- 05-composition-api/src/App.vue
+++ 05-composition-api/src/App.vue
@@ -3,6 +3,16 @@
 		<h2 v-if="visible">안녕하세요!</h2>
 		<h2 v-else>false 입니다</h2>
 		<button @click="visible = !visible">토글</button>
+		<hr />
+		<button v-on:click="type = 'A'">A</button>
+		<button v-on:click="type = 'B'">B</button>
+		<button v-on:click="type = 'C'">C</button>
+		<button v-on:click="type = 'D'">D</button>
+
+		<h2 v-if="type === 'A'">A입니다.</h2>
+		<h2 v-else-if="type === 'B'">B입니다.</h2>
+		<h2 v-else-if="type === 'C'">C입니다.</h2>
+		<h2 v-else>A, B, C가 아닙니다.</h2>
 	</div>
 </template>
 
@@ -11,10 +21,12 @@ import { ref } from 'vue';
 
 export default {
 	setup() {
-		const visible = ref(true);
+		const visible = ref(false);
+		const type = ref('A');
 
 		return {
 			visible,
+			type,
 		};
 	},
 };
