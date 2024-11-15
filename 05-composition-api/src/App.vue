2b69a81e3b3b61899510ef770842ec25793db5e9
LifeIsOne <didtmdgh258@gmail.com>
2024-11-15 19:09:38 +0900

    [FEAT] v-for 디렉티브 활용하여 목록 렌더링

--- 05-composition-api/src/App.vue
+++ 05-composition-api/src/App.vue
@@ -1,9 +1,32 @@
 <template>
-	<div></div>
+	<div>
+		<ul>
+			<template v-for="(item, index) in items" :key="item.id">
+				<li v-if="item.id % 2 === 0">
+					ID : {{ item.id }}, 인덱스 : {{ index }} , {{ item.msg }}
+				</li>
+			</template>
+		</ul>
+	</div>
 </template>
 
 <script>
-export default {};
+import { reactive } from 'vue';
+
+export default {
+	setup() {
+		const items = reactive([
+			{ id: 1, msg: 'Java' },
+			{ id: 2, msg: 'Html' },
+			{ id: 3, msg: 'Css' },
+			{ id: 4, msg: 'JavaScript' },
+		]);
+
+		return {
+			items,
+		};
+	},
+};
 </script>
 
 <style lang="scss" scoped></style>
