89e8b6f529ed395781897d4b886d3dc6f7396373
LifeIsOne <didtmdgh258@gmail.com>
2024-11-17 21:47:18 +0900

    [FEAT]v-text, v-html, v-cloak 디렉티브

--- 05-composition-api/src/App.vue
+++ 05-composition-api/src/App.vue
@@ -1,11 +1,24 @@
 <template>
-	<div></div>
+	<div>
+		<p>{{ msg }}</p>
+		<p v-text="msg"></p>
+		<p v-html="htmlString"></p>
+		<p v-text="htmlString"></p>
+	</div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
 	setup() {
-		return {};
+		const msg = ref('안녕하세요');
+		const htmlString = ref('<String>반가워!</String>');
+
+		return {
+			msg,
+			htmlString,
+		};
 	},
 };
 </script>
