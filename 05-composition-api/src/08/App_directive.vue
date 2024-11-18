b091cb133d108d504d4263509c46514e91a342b9
LifeIsOne <didtmdgh258@gmail.com>
2024-11-18 17:22:13 +0900

    [FEAT] v-once 디렉티브

---
+++ 05-composition-api/src/08/App_directive.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<p>{{ msg }}</p>
+		<p v-text="msg"></p>
+		<p v-html="htmlString"></p>
+		<p v-text="htmlString"></p>
+		<p v-once>{{ msg }}</p>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		const msg = ref('안녕하세요');
+		const htmlString = ref('<String>반가워!</String>');
+
+		return {
+			msg,
+			htmlString,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
+<style>
+@import './assets/base.css';
+</style>
--- 05-composition-api/src/App.vue
+++ 05-composition-api/src/App.vue
@@ -1,24 +1,11 @@
 <template>
-	<div>
-		<p>{{ msg }}</p>
-		<p v-text="msg"></p>
-		<p v-html="htmlString"></p>
-		<p v-text="htmlString"></p>
-	</div>
+	<div></div>
 </template>
 
 <script>
-import { ref } from 'vue';
-
 export default {
 	setup() {
-		const msg = ref('안녕하세요');
-		const htmlString = ref('<String>반가워!</String>');
-
-		return {
-			msg,
-			htmlString,
-		};
+		return {};
 	},
 };
 </script>
