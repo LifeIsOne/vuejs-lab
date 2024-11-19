3d9c45485fe1d7f501d31830901ebca411934d4f
LifeIsOne <didtmdgh258@gmail.com>
2024-11-20 02:36:05 +0900

    [FEAT] v-memo 디렉티브

---
+++ 05-composition-api/src/08/App_v-memo.vue
@@ -0,0 +1,44 @@
+<template>
+	<div>
+		<!-- memo에 아무것도 안넣기 -->
+		<h1>v-memo="[]"</h1>
+		<div v-memo="[]">
+			<p>subscribers : {{ subscribers }}</p>
+			<p>views : {{ views }}</p>
+			<p>likes : {{ likes }}</p>
+		</div>
+		<button @click="subscribers++">sub++</button>
+		<button @click="views++">views++</button>
+		<button @click="likes++">likes++</button>
+		<!-- v-memo에 반응형 데이터 넣기 -->
+		<h1>v-memo="[subscribers]"</h1>
+		<div v-memo="[subscribers]">
+			<p>subscribers : {{ subscribers }}</p>
+			<p>views : {{ views }}</p>
+			<p>likes : {{ likes }}</p>
+		</div>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		const subscribers = ref(100);
+		const views = ref(1000);
+		const likes = ref(10);
+
+		return {
+			subscribers,
+			views,
+			likes,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
+<style>
+@import './assets/base.css';
+</style>
