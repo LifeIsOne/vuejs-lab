379970ab0af72f2698a8d6f688ccf6586d5bd733
LifeIsOne <didtmdgh258@gmail.com>
2024-11-22 23:56:22 +0900

    [FEAT] 템플릿에서 이벤트 핸들러 작성 및 이벤트 객체 접근

---
+++ 05-composition-api/src/09/App_event.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<button @click="printEventInfo('안녕하세요', $event)">
+			inline event handler
+		</button>
+		<hr />
+		<input type="text" @keyup="onKeyupHandler" />
+	</div>
+</template>
+
+<script>
+export default {
+	setup() {
+		const printEventInfo = (msg, event) => {
+			console.log('msg', msg);
+			console.log('event.target', event.target);
+			console.log('event.target.tagName', event.target.tagName);
+		};
+		const onKeyupHandler = () => {
+			console.log('event.key', event.key);
+		};
+
+		return {
+			printEventInfo,
+			onKeyupHandler,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
+<style>
+@import './assets/base.css';
+</style>
