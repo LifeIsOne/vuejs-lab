d8984acf6204e078606e6973e2817c63fe480cc1
LifeIsOne <didtmdgh258@gmail.com>
2024-11-23 01:16:47 +0900

    [FEAT] .enter 키 수식어

--- 05-composition-api/src/App.vue
+++ 05-composition-api/src/App.vue
@@ -1,11 +1,32 @@
 <template>
-	<div></div>
+	<div>
+		<input type="text" @keyup.enter="addTodo" />
+		<ul>
+			<li v-for="(todo, index) in todos" :key="index">
+				{{ todo }}
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
+import { reactive } from 'vue';
+
 export default {
 	setup() {
-		return {};
+		const todos = reactive([]);
+
+		const addTodo = event => {
+			// console.log('event.key', event.key);
+			// if (event.key === 'Enter') {
+			todos.push(event.target.value);
+			// }
+		};
+
+		return {
+			todos,
+			addTodo,
+		};
 	},
 };
 </script>
