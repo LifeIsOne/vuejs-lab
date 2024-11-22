49f5caf7544a05d01cd031ede91380290e06dfa6
LifeIsOne <didtmdgh258@gmail.com>
2024-11-23 00:29:34 +0900

    [FEAT] 이벤트 수식어  Modifiers 사용하기, 이벤트 버블링 이해

--- 05-composition-api/src/App.vue
+++ 05-composition-api/src/App.vue
@@ -1,11 +1,45 @@
 <template>
-	<div></div>
+	<div>
+		<div id="modifiers">
+			<div @click="clickDiv">
+				div 영역
+				<p @click="clickP">
+					p영역
+					<span @click.stop="clickSpan"> span영역 </span>
+					<a href="https://naver.com" @click.prevent.stop="clickA"
+						>a영역 naver웹페이지 이동하기</a
+					>
+				</p>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
 	setup() {
-		return {};
+		const clickDiv = () => {
+			console.log('clickDiv');
+			location.href =
+				'https://elated-value-48d.notion.site/15-VueJS-Vue3-11ad91c8ac0e8066be2ae42692a74129?pvs=4';
+		};
+		const clickP = () => {
+			console.log('clickP');
+		};
+		const clickSpan = () => {
+			console.log('clickSpan');
+			alert('좋아요를 누르셨습니다!');
+		};
+		const clickA = () => {
+			alert('a태그를 누르셨습니다.');
+		};
+
+		return {
+			clickDiv,
+			clickP,
+			clickSpan,
+			clickA,
+		};
 	},
 };
 </script>
@@ -13,4 +47,19 @@ export default {
 <style lang="scss" scoped></style>
 <style>
 @import './assets/base.css';
+#modifiers div,
+#modifiers p,
+#modifiers span {
+	padding: 40px;
+}
+#modifiers div {
+	background: #999;
+}
+#modifiers p {
+	background: #666;
+}
+#modifiers span {
+	background: #333;
+	display: block;
+}
 </style>
