bed9499f8c43c0b7b04266827cc291125f3be982
LifeIsOne <didtmdgh258@gmail.com>
2024-11-22 23:25:05 +0900

    [FEAT] 커스텀 디렉티브 맛보기

---
+++ 05-composition-api/src/08/App_custom_directive.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<p>포커스 되는 지 확인!@</p>
+		<input type="text" v-focus />
+	</div>
+</template>
+
+<script>
+export default {
+	setup() {
+		return {};
+	},
+	directives: {
+		focus: {
+			mounted(el) {
+				el.focus();
+			},
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
+<style>
+@import './assets/base.css';
+</style>
