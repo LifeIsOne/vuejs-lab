c1255e10da3b36b2af5e41d6d5c1161f99ea5808
LifeIsOne <didtmdgh258@gmail.com>
2024-11-23 19:43:16 +0900

    [FEAT]  키보드 수식어

---
+++ 05-composition-api/src/09/App_keyboard_event.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<input type="text" @keyup.enter.ctrl="addTodo" />
+		<ul>
+			<li v-for="(todo, index) in todos" :key="index">
+				{{ todo }}
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+	setup() {
+		const todos = reactive([]);
+
+		const addTodo = event => {
+			// console.log('event.key', event.key);
+			// if (event.key === 'Enter') {
+			todos.push(event.target.value);
+			event.target.value = '';
+			// }
+		};
+
+		return {
+			todos,
+			addTodo,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
+<style>
+@import './assets/base.css';
+</style>
--- 05-composition-api/src/App.vue
+++ 05-composition-api/src/App.vue
@@ -1,32 +1,11 @@
 <template>
-	<div>
-		<input type="text" @keyup.enter="addTodo" />
-		<ul>
-			<li v-for="(todo, index) in todos" :key="index">
-				{{ todo }}
-			</li>
-		</ul>
-	</div>
+	<div></div>
 </template>
 
 <script>
-import { reactive } from 'vue';
-
 export default {
 	setup() {
-		const todos = reactive([]);
-
-		const addTodo = event => {
-			// console.log('event.key', event.key);
-			// if (event.key === 'Enter') {
-			todos.push(event.target.value);
-			// }
-		};
-
-		return {
-			todos,
-			addTodo,
-		};
+		return {};
 	},
 };
 </script>
