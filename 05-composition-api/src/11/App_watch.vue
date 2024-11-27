89e1b6191eadd1ef38e76a4a52ca2c1826c62882
LifeIsOne <didtmdgh258@gmail.com>
2024-11-27 20:46:40 +0900

    [FEA] watch 함수

---
+++ 05-composition-api/src/11/App_watch.vue
@@ -0,0 +1,28 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { ref, watch } from 'vue';
+
+export default {
+	setup() {
+		const msg = ref('');
+		watch(msg, (newValue, oldValue) => {
+			console.log('newValue', newValue);
+			console.log('oldValue', oldValue);
+		});
+
+		return {
+			msg,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
+
+<style lang="scss" scoped></style>
+<style>
+@import './assets/base.css';
+</style>
--- 05-composition-api/src/App.vue
+++ 05-composition-api/src/App.vue
@@ -3,9 +3,44 @@
 </template>
 
 <script>
+import { ref, watch } from 'vue';
+
 export default {
 	setup() {
-		return {};
+		const msg = ref('');
+		watch(msg, (newValue, oldValue) => {
+			// DOM -
+
+			// API -
+
+			// 사ㅇ태 변경
+			console.log('newValue', newValue);
+			console.log('oldValue', oldValue);
+		});
+
+		// const x = ref(0);
+		// const y = ref(0);
+
+		// watch(x, () => {
+		// 	console.log('x 는 ${newX}');
+		// });
+
+		// // getter 함수
+		// watch(
+		// 	() => x.value + y.value,
+		// 	sum => {
+		// 		console.log('x + y = ${sum}');
+		// 	},
+		// );
+
+		// // array 배열
+		// watch([x, () => y.value], ([newX, newY]) => {
+		// 	console.log(`x is ${newX} and y is ${newY}`);
+		// });
+
+		return {
+			msg,
+		};
 	},
 };
 </script>
