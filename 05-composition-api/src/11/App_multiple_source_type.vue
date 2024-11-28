cc18827b822ea8037dad880294b04b76aa877782
LifeIsOne <didtmdgh258@gmail.com>
2024-11-28 20:07:24 +0900

    [FEAT] watch함수 깊은 감시 예제

---
+++ 05-composition-api/src/11/App_multiple_source_type.vue
@@ -0,0 +1,78 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { reactive, ref, watch } from 'vue';
+
+export default {
+	setup() {
+		const x = ref(0);
+		const y = ref(0);
+
+		// //# getter 함수
+		// watch(
+		// 	() => x.value + y.value,
+		// 	sum => {
+		// 		console.log('x + y = ', sum);
+		// 	},
+		// );
+
+		// // # array 배열
+		// watch([x, y], ([newX, newY]) => {
+		// 	console.log('newX :', +newX, 'newY :', +newY);
+		// });
+
+		// //# object
+		const obj = reactive({
+			count: 0,
+		});
+
+		// watch(obj, (newValue, oldValue) => {
+		// 	console.log('newValue : ', newValue, 'oldValue : ', oldValue);
+		// });
+
+		// // object의 하나의 속성만 감시 - getter함수
+		// watch(
+		// 	() => obj.count,
+		// 	(newValue, oldValue) => {
+		// 		console.log('newValue : ', newValue, 'oldValue : ', oldValue);
+		// 	},
+		// );
+
+		// # deep option
+		const user = reactive({
+			name: 'Matthew',
+			age: 29,
+			hobby: 'reading',
+			obj: {
+				count: 0,
+			},
+		});
+
+		// watch(user, newValue => {
+		// 	console.log('newValue: ', newValue);
+		// });
+
+		watch(
+			() => user.obj,
+			newValue => {
+				console.log('newValue: ', newValue);
+			},
+		);
+		return {
+			x,
+			y,
+			obj,
+			user,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
+
+<style lang="scss" scoped></style>
+<style>
+@import './assets/base.css';
+</style>
--- 05-composition-api/src/App.vue
+++ 05-composition-api/src/App.vue
@@ -3,65 +3,12 @@
 </template>
 
 <script>
-import { reactive, ref, watch } from 'vue';
-
 export default {
 	setup() {
-		const x = ref(0);
-		const y = ref(0);
-
-		// //# getter 함수
-		// watch(
-		// 	() => x.value + y.value,
-		// 	sum => {
-		// 		console.log('x + y = ', sum);
-		// 	},
-		// );
-
-		// # array 배열
-		// watch([x, y], ([newX, newY]) => {
-		// 	console.log('newX :', +newX, 'newY :', +newY);
-		// });
-
-		//# object
-		const obj = reactive({
-			count: 0,
-		});
-
-		// watch(obj, (newValue, oldValue) => {
-		// 	console.log('newValue : ', newValue, 'oldValue : ', oldValue);
-		// });
-
-		// // object의 하나의 속성만 감시 - getter함수
-		// watch(
-		// 	() => obj.count,
-		// 	(newValue, oldValue) => {
-		// 		console.log('newValue : ', newValue, 'oldValue : ', oldValue);
-		// 	},
-		// );
-
-		const user = reactive({
-			name: 'Matthew',
-			age: 29,
-			hobby: 'reading',
-			obj: {
-				count: 0,
-			},
-		});
-
-		watch(user, newValue => {
-			console.log('newValue: ', newValue);
-		});
-		return {
-			// x,
-			// y,
-			obj,
-		};
+		return {};
 	},
 };
 </script>
-
-<style lang="scss" scoped></style>
 
 <style lang="scss" scoped></style>
 <style>
