67f4bd5064311ad111e5166c7afbb02d116a39ac
LifeIsOne <didtmdgh258@gmail.com>
2024-11-28 21:10:07 +0900

    [FEAT] watchEffect 함수 사용하기

---
+++ 05-composition-api/src/11/App_watchEffect.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<form @submit.prevent="">
+			<input v-model.lazy="title" type="text" placeholder="Title" />
+			<textarea v-model.lazy="contents" placeholder="Contents"></textarea>
+			<hr />
+			<button @click="save(title, contents)">저장하기</button>
+		</form>
+	</div>
+</template>
+
+<script>
+import { ref, watchEffect } from 'vue';
+
+export default {
+	setup() {
+		const title = ref('');
+		const contents = ref('');
+
+		const save = (title, contents) => {
+			console.log(`저장됐습니다. title : ${title}, contents : ${contents}`);
+		};
+
+		watchEffect(() => {
+			// console.log('title : ' + title.value);
+			// console.log('contents : ' + contents.value);
+			save(title.value, contents.value);
+		});
+
+		return {
+			title,
+			contents,
+			save,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
+<style>
+@import './assets/base.css';
+</style>
