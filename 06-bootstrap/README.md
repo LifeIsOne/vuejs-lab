00065806a1ca9d2f79076580c00fb33f9ff1dda4
LifeIsOne <didtmdgh258@gmail.com>
2024-12-01 21:39:08 +0900

    [FIX] 'BootstrapVueNext'를 사용하여 Vue3와 Bootstrap5 호환성 문제 해결

--- 06-bootstrap/README.md
+++ 06-bootstrap/README.md
@@ -1,4 +1,4 @@
-# 06-bootstrap-vue
+# 06-bootstrap
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- 06-bootstrap/vite.config.js
+++ 06-bootstrap/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+// BootstrapVueNext
 import Components from 'unplugin-vue-components/vite';
 import { BootstrapVueNextResolver } from 'bootstrap-vue-next';
 
+// https://vite.dev/config/
 export default defineConfig({
 	plugins: [
 		vue(),
+		vueDevTools(),
 		Components({
 			resolvers: [BootstrapVueNextResolver()],
 		}),
