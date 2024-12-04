265b05b1c5b5921f2dd80cbfba1738669aff2672
LifeIsOne <didtmdgh258@gmail.com>
2024-12-04 20:41:34 +0900

    [REMOVE] 프로젝트 정리

---
+++ 06-bootstrap/src/components/TheView.vue
@@ -0,0 +1,117 @@
+<template>
+	<main>
+		<div class="container py-5">
+			<BContainer class="bv-example-row">
+				<BRow class="g-2">
+					<!-- <BRow gutter-y="10"> -->
+					<BCol cols="4">
+						<BCard
+							title="Card Title"
+							img-src="https://picsum.photos/id/25/600/300"
+							img-alt="Image"
+							img-top
+							tag="article"
+							style="max-width: 20rem"
+						>
+							<BCardText>
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</BCardText>
+							<BButton href="#" variant="dark">Go somewhere</BButton>
+						</BCard>
+					</BCol>
+					<BCol cols="4">
+						<BCard
+							title="Card Title"
+							img-src="https://picsum.photos/id/25/600/300"
+							img-alt="Image"
+							img-top
+							tag="article"
+							style="max-width: 20rem"
+						>
+							<BCardText>
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</BCardText>
+							<BButton href="#" variant="dark">Go somewhere</BButton>
+						</BCard>
+					</BCol>
+					<BCol cols="4">
+						<BCard
+							title="Card Title"
+							img-src="https://picsum.photos/id/25/600/300"
+							img-alt="Image"
+							img-top
+							tag="article"
+							style="max-width: 20rem"
+						>
+							<BCardText>
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</BCardText>
+							<BButton href="#" variant="dark">Go somewhere</BButton>
+						</BCard>
+					</BCol>
+					<BCol cols="4">
+						<BCard
+							title="Card Title"
+							img-src="https://picsum.photos/id/25/600/300"
+							img-alt="Image"
+							img-top
+							tag="article"
+							style="max-width: 20rem"
+						>
+							<BCardText>
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</BCardText>
+							<BButton href="#" variant="dark">Go somewhere</BButton>
+						</BCard>
+					</BCol>
+					<BCol cols="4">
+						<BCard
+							title="Card Title"
+							img-src="https://picsum.photos/id/25/600/300"
+							img-alt="Image"
+							img-top
+							tag="article"
+							style="max-width: 20rem"
+						>
+							<BCardText>
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</BCardText>
+							<BButton href="#" variant="dark">Go somewhere</BButton>
+						</BCard>
+					</BCol>
+					<BCol cols="4">
+						<BCard
+							title="Card Title"
+							img-src="https://picsum.photos/id/25/600/300"
+							img-alt="Image"
+							img-top
+							tag="article"
+							style="max-width: 20rem"
+						>
+							<BCardText>
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</BCardText>
+							<BButton href="#" variant="dark">Go somewhere</BButton>
+						</BCard>
+					</BCol>
+				</BRow>
+			</BContainer>
+		</div>
+	</main>
+</template>
+
+<script>
+export default {
+	setup() {
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
