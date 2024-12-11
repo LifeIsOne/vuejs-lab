df1ae8ecb42fad3c79c63db373bd7b59254d05dd
LifeIsOne <didtmdgh258@gmail.com>
2024-12-11 17:21:14 +0900

    [FEAT] ``$style``내장 객체 이용하여 Css클래스 적용하기

--- 06-component/src/components/AppCard.vue
+++ 06-component/src/components/AppCard.vue
@@ -17,8 +17,9 @@
 	<!-- Bootstrap -->
 	<div class="card">
 		<div class="card-body">
+			{{ $style }}
 			<h5 class="card-title red">Card Title</h5>
-			<p class="card-text">
+			<p class="card-text" :class="$style.red">
 				좋아하는 직업을 정하면 평생 단 하루도 일하지 않는 것과 같다. - 공자
 			</p>
 			<a href="#" class="btn btn-dark">더보기</a>
@@ -34,7 +35,15 @@ export default {
 };
 </script>
 
-<style scoped>
+<!-- scoped 속성 -->
+<!-- <style scoped>
+.red {
+	color: red !important;
+}
+</style> -->
+
+<!-- module 속성 -->
+<style module>
 .red {
 	color: red !important;
 }
