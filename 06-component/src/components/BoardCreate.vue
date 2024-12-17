10e86775df0e9973b036061903d4d9a1d0ae2a6a
LifeIsOne <didtmdgh258@gmail.com>
2024-12-17 18:33:10 +0900

    [FEAT] `setup()`두 번째 이자 `context`의 `emit()`속성 이용하기

--- 06-component/src/components/BoardCreate.vue
+++ 06-component/src/components/BoardCreate.vue
@@ -1,18 +1,27 @@
 <template>
 	<div>
-		<button
+		<!-- 하위 컴포넌트에서 상위 컴포넌트 이벤트 발생시키기 -->
+		<!-- 인스턴스 내장된 $emit()메서드 사용 -->
+		<!-- <button
 			class="btn btn-primary"
 			@click="$emit('createBoard', 1, 2, 3, 'Library')"
 		>
 			BUTTON
-		</button>
+		</button> -->
+
+		<!-- CompositionAPI의 setup()의 두 번째 파라미터 context의 emit()속성 이용하기 -->
+		<button class="btn btn-success" @click="createBoard">BUTTON</button>
 	</div>
 </template>
 
 <script>
 export default {
-	setup() {
-		return {};
+	setup(props, { emit }) {
+		const createBoard = () => {
+			emit('createBoard', 1, 2, 3, 'Library');
+		};
+
+		return { createBoard };
 	},
 };
 </script>
