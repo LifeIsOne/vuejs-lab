3fd9f9ac5dd548123e6f4a96f4bb63a2e6c350ee
LifeIsOne <didtmdgh258@gmail.com>
2024-12-22 21:24:30 +0900

    [FEAT] Computed 속성으로 v-model 바인딩 구현

--- 06-component/src/components/LabelInput.vue
+++ 06-component/src/components/LabelInput.vue
@@ -2,21 +2,29 @@
 	<label>
 		{{ label }}
 		<!-- <input v-model="username" type="text" /> -->
-		<input
-			:value="modelValue"
-			@input="event => $emit('update:modelValue', event.target.value)"
-			type="text"
-		/>
+		<!-- :value="modelValue"
+    @input="event => $emit('update:modelValue', event.target.value)" -->
+		<input v-model="value" type="text" />
 	</label>
 </template>
 
 <script>
+import { computed } from 'vue';
+
 export default {
 	props: ['modelValue', 'label'],
 	emits: ['update:modelValue'],
 
-	setup() {
-		return {};
+	setup(props, context) {
+		const value = computed({
+			get() {
+				return props.modelValue;
+			},
+			set(newValue) {
+				context.emit('update:modelValue', newValue);
+			},
+		});
+		return { value };
 	},
 };
 </script>
