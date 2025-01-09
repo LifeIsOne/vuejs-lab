5fe933e15cc4dee3cad3fa655c01fba93609fc7f
LifeIsOne <didtmdgh258@gmail.com>
2025-01-09 19:22:39 +0900

    [FEAT] `setup()`에서 `slot`접근하여 여부 확인하기

--- 06-component/src/components/AppCard.vue
+++ 06-component/src/components/AppCard.vue
@@ -6,20 +6,20 @@
 		<div class="card-body">
 			<slot :child-msg hello-world="Hello World !">#BODY</slot>
 		</div>
-		<div v-if="$slots.footer" class="card-footer text-muted">
+		<div v-if="hasFooter" class="card-footer text-muted">
 			<slot name="footer" footer-msg="푸터 메시지"></slot>
 		</div>
 	</div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 export default {
-	setup() {
+	setup(props, { slots }) {
 		const childMsg = ref('하위 컴포넌트 메시지');
-
-		return { childMsg };
+		const hasFooter = computed(() => !!slots.footer);
+		return { childMsg, hasFooter };
 	},
 };
 </script>
