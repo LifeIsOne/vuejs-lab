72ca34690254c848f4eb87ae574af5e9ed3176a3
LifeIsOne <didtmdgh258@gmail.com>
2025-01-10 19:36:52 +0900

    [FEAT] Provide, Inject 실습을 위한 컴포넌트 생성

---
+++ 06-component/src/components/ChildComponent.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="card">
+		<div class="card-header">Child Component</div>
+		<div class="card-body">
+			<DeepChildComponent></DeepChildComponent>
+		</div>
+	</div>
+</template>
+
+<script>
+import DeepChildComponent from '@/components/DeepChildComponent.vue';
+
+export default {
+	components: {
+		DeepChildComponent,
+	},
+
+	setup() {
+		return { DeepChildComponent };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
--- 06-component/src/components/ProvideInject.vue
+++ 06-component/src/components/ProvideInject.vue
@@ -1,11 +1,18 @@
 <template>
-	<div class="container py-5"></div>
+	<div class="container py-5">
+		<ChildComponent></ChildComponent>
+	</div>
 </template>
 
 <script>
+import ChildComponent from '@/components/ChildComponent.vue';
+
 export default {
+	components: {
+		ChildComponent,
+	},
 	setup() {
-		return {};
+		return { ChildComponent };
 	},
 };
 </script>
