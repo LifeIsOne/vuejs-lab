7d3a071392ea6e9505488c408db85104b5f92a69
LifeIsOne <didtmdgh258@gmail.com>
2025-01-15 23:29:56 +0900

    [FEAT] `ref`속성과 `Mountied Hook`을 활용해 `DOM객체`참조하기

--- 06-component/src/components/LifecycleHooks.vue
+++ 06-component/src/components/LifecycleHooks.vue
@@ -1,22 +1,25 @@
 <template>
-	<div class="contaion py-5"></div>
+	<div class="contaion py-5">
+		<input ref="inputRef" type="text" />
+	</div>
 </template>
 
 <script>
-import { onBeforeMount, onMounted } from 'vue';
+import { onBeforeMount, onMounted, ref } from 'vue';
 
 export default {
 	setup() {
 		console.log('setup');
+		const inputRef = ref(null);
 
 		onMounted(() => {
-			console.log('onMounted');
+			console.log('onMounted', inputRef.value);
 		});
 		onBeforeMount(() => {
-			console.log('onBeforeMount');
+			console.log('onBeforeMount', inputRef.value);
 		});
 
-		return {};
+		return { inputRef };
 	},
 	// beforeCreate() {
 	// 	console.log('beforeCreate');
