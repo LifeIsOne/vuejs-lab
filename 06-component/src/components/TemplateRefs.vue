e3e20d33b0861c131c2b8770525a201891e9329a
LifeIsOne <didtmdgh258@gmail.com>
2025-01-23 23:14:37 +0900

    [FEAT] `ref()`를 사용해 `DOM`요소에 접근하기

--- 06-component/src/components/TemplateRefs.vue
+++ 06-component/src/components/TemplateRefs.vue
@@ -1,11 +1,23 @@
 <template>
 	<div class="container py-5"></div>
+	<input ref="input" type="text" />
 </template>
 
 <script>
+import { onMounted, ref } from 'vue';
+
 export default {
 	setup() {
-		return {};
+		const input = ref(null);
+
+		// 마운트되기 이전이기 때문에 `null`인 상태
+		console.log('setup: ', input.value);
+
+		onMounted(() => {
+			console.log('onMounted: ', input.value);
+		});
+
+		return { input };
 	},
 };
 </script>
