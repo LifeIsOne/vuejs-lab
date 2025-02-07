e245ca56bc99b17028fe7f3880128aa8c4e1dfa7
LifeIsOne <didtmdgh258@gmail.com>
2025-02-07 23:40:05 +0900

    [FEAT] `<script setup>`문법

---
+++ 06-component/src/components/ScriptSetup.vue
@@ -0,0 +1,16 @@
+<template>
+	<div></div>
+</template>
+
+<script setup>
+console.log('안녕하세요');
+</script>
+<script>
+export default {
+	setup() {
+		console.log('안녕하세요');
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
