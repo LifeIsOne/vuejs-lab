8f59e5b9b827d7e04ffb5f23ac9745a5909cd14e
LifeIsOne <didtmdgh258@gmail.com>
2025-02-08 00:16:10 +0900

    [FEAT] 반응형 상태 및 함수 추가

--- 06-component/src/components/ScriptSetup.vue
+++ 06-component/src/components/ScriptSetup.vue
@@ -1,9 +1,22 @@
 <template>
-	<div>{{ msg }}</div>
+	<div>
+		<p>{{ msg }}</p>
+		<p>{{ refMsg }}</p>
+		<input type="text" v-model="refMsg" />
+		<div>
+			<button @click="alertHello">클릭</button>
+		</div>
+	</div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
 const msg = '안녕하세요';
+const refMsg = ref('');
+const alertHello = () => {
+	alert('안녕하세요👋');
+};
 </script>
 
 <style lang="scss" scoped></style>
