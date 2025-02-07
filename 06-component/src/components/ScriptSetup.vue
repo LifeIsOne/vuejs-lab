b17c42f4e9e2b9a14273436377c6afabff4638e9
LifeIsOne <didtmdgh258@gmail.com>
2025-02-08 00:29:32 +0900

    [FEAT] `<script setup>` 속성에 컴포넌트 등록 - `components` 옵션 생략

--- 06-component/src/components/ScriptSetup.vue
+++ 06-component/src/components/ScriptSetup.vue
@@ -1,16 +1,27 @@
 <template>
-	<div>
+	<div class="container py-5">
+		<!-- 변수 -->
 		<p>{{ msg }}</p>
+		<!-- 반응형 상태 양방향 바인딩 -->
 		<p>{{ refMsg }}</p>
 		<input type="text" v-model="refMsg" />
+		<!-- 함수 -->
 		<div>
 			<button @click="alertHello">클릭</button>
 		</div>
+		<!-- 컴포넌트 -->
+		<PostItem
+			type="new"
+			title="제목1"
+			contents="내용1"
+			:is-like="true"
+		></PostItem>
 	</div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import PostItem from '@/components/setup/PostItem.vue';
 
 const msg = '안녕하세요';
 const refMsg = ref('');
---
+++ 06-component/src/components/setup/PostItem.vue
@@ -0,0 +1,92 @@
+<template>
+	<!-- BootstrapVueNext -->
+	<BBadge class="mb-2">{{ typeName }}</BBadge>
+	<BCard
+		:title="title"
+		img-src="https://picsum.photos/id/25/600/300"
+		img-alt="Image"
+		img-top
+		tag="article"
+		class="bg-dark"
+		style="color: #fff8; border-color: #777"
+	>
+		<BCardText>
+			{{ contents }}
+		</BCardText>
+		<a
+			href="#"
+			class="btn"
+			:class="isLikeClass"
+			variant="dark"
+			@click="likeToggle"
+			>❤︎</a
+		>
+	</BCard>
+
+	<!-- Bootstrap -->
+	<!-- <div class="card">
+		<div class="card-body">❤❤
+			<h5 class="card-title red">{{ title }}</h5>
+			<p class="card-text">
+				{{ contents }}
+			</p>
+			<a href="#" class="btn btn-dark">더보기</a>
+		</div>
+	</div> -->
+</template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+	props: {
+		type: {
+			type: String,
+			default: 'news',
+			validator: value => {
+				return ['news', 'notice'].includes(value);
+			},
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		contents: {
+			type: String,
+			// required: true,
+		},
+		isLike: {
+			type: Boolean,
+			default: false,
+		},
+		// Reference 타입의 defalut는 기본값을 반환하는 팩토리 함수를 설정해야 합니다.
+		obj: {
+			type: Object,
+			default: () => ({}),
+		},
+	},
+	emits: ['likeToggle'],
+	// setup함수의 첫번째 매개변수로 props를 받을 수 있습니다.
+	setup(props, context) {
+		// console.log('props.title : ', props.title);
+		const isLikeClass = computed(() =>
+			props.isLike ? 'btn-primary' : 'btn-outline-primary',
+		);
+		const typeName = computed(() =>
+			props.type === 'news' ? 'News' : 'Notice',
+		);
+		const likeToggle = () => {
+			// props.isLke = !props.isLike;
+			context.emit('likeToggle');
+		};
+
+		return {
+			isLikeClass,
+			typeName,
+			likeToggle,
+		};
+	},
+};
+</script>
+
+<style></style>
