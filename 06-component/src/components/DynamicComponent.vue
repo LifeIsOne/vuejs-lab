e33112c09bee45d6d8ed06905956dcf1433ab66b
LifeIsOne <didtmdgh258@gmail.com>
2025-04-15 21:45:43 +0900

    [FEAT] `<component>`태그와 `v-bind:is`속성을 사용하여 동적 컴포넌트 구현

---
+++ 06-component/src/components/DynamicComponent.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="container py-4">
+		<button
+			class="btn btn-danger me-2"
+			@click="changeCurrentComp(DynamicApple)"
+		>
+			Apple
+		</button>
+		<button class="btn btn-warning" @click="changeCurrentComp(DynamicBanana)">
+			Banana
+		</button>
+		<hr />
+		<component :is="currentComp"></component>
+	</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import DynamicApple from './DynamicApple.vue';
+import DynamicBanana from './DynamicBanana.vue';
+
+const currentComp = ref(DynamicApple);
+const changeCurrentComp = comp => (currentComp.value = comp);
+</script>
+
+<style lang="scss" scoped></style>
