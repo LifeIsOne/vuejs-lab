01620add2a054458470dc6c1a8cb391393d8bb68
LifeIsOne <didtmdgh258@gmail.com>
2025-04-15 19:15:43 +0900

    [FEAT] `MyButton.vue` 에 `<script setup>`적용,

--- 06-component/src/components/setup/MyButton.vue
+++ 06-component/src/components/setup/MyButton.vue
@@ -2,29 +2,20 @@
 	<div class="bg-dark p-3">
 		<!-- <button class="btn btn-secondary" type="button" v-bind="$attrs">MY BUTTON</button> -->
 		<button class="btn btn-secondary" type="button" @click="helloWorld">
-			MY BUTTON
+			MY BUTTON {{ msg }}
 		</button>
 	</div>
 </template>
 
 <script>
 export default {
-	// inheritAttrs: false,
-	emits: ['click'],
-
-	setup(props, context) {
-		// console.log('context.attrs: ', context.attrs);
-		// console.log('class	: ', context.attrs.class);
-		// console.log('id		: ', context.attrs.id);
-		// console.log('onClick	: ', context.attrs.onClick);
-
-		// context.attrs.onClick();
-		const helloWorld = () => {
-			context.emit('click');
-		};
-		return { helloWorld };
-	},
+	inheritAttrs: false,
 };
+</script>
+<script setup>
+import { ref } from 'vue';
+
+const msg = ref('msg');
 </script>
 
 <style lang="scss" scoped></style>
